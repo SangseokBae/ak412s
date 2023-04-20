e7c0f07b25c5d2b47a19f00b9baea312ca2b90eb
SangseokBae <ssb9018@ajou.ac.kr>
2023-04-20 13:13:29 +0900

    first commit

--- R/mkNAs.R
+++ R/mkNAs.R
@@ -1,4 +1,4 @@
-mkna<-function(tmp_data, col_number, NAs_number){
+mkNAs<-function(tmp_data, col_number, NAs_number){
 n<-nrow(tmp_data)
 selRows<-sample(1:n, NAs_number, replace=F)
 tmp_data[selRows, c(col_number)]<-NA
